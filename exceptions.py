a05a0189d049017eca0d4c85a1011bc6b11be5a8
Rona Godilo <ronagodilo05@gmail.com>
2026-05-20 19:56:24 +0800

    Create exceptions.py
    Add exceptions.py file

---
+++ exceptions.py
@@ -0,0 +1,11 @@
+class BookNotFoundError(Exception):
+    pass
+
+class MemberNotFoundError(Exception):
+    pass
+
+class BookUnavailableError(Exception):
+    pass
+
+class LoanNotFoundError(Exception):
+    pass
